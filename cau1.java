a88c3d07bb143c9085573cda2354303482fafd19
TonyP <124651841+TonyPHam09@users.noreply.github.com>
2023-02-18 01:25:30 +0700

    Update cau1.java

--- cau1.java
+++ cau1.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class cau1 {
-    public static boolean checkPerfectSquare(Integer n) /*dữ kiện quan trọng để tính sqrt*/
-    {                                                  //
-        double sqrt;                                   //
-        sqrt = Math.sqrt(n);                           //*phần này để kiểm tra số nguyên khi thực hiên căn bậc 2*/
-        return ((sqrt - Math.floor(sqrt)) == 0);       //
-    }                                                  /*xét căn bậc không cần bước giải*/
+    public static boolean checkPerfectSquare(Integer n)
+    {                                                  
+        double sqrt;                                   
+        sqrt = Math.sqrt(n);                           
+        return ((sqrt - Math.floor(sqrt)) == 0);       
+    }                                                  
     public static void main(String[] args)
     {
         int n;
