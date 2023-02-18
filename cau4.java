f037bba6a5ed614a948cdf1825abe48b59d30163
TonyP <124651841+TonyPHam09@users.noreply.github.com>
2023-02-19 00:39:27 +0700

    Add files via upload

---
+++ cau4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class cau4 {
+     public static void main(String[] args) {
+        int n,i,s,temp;
+        int arr[];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap mang: ");
+        n = sc.nextInt();
+        arr = new int[n];
+        for (i = 0; i < n; i++) {
+            System.out.println("nhap phan tu thu " + i + ": ");
+            arr[i] = sc.nextInt();
+        }
+        for (i = 0; i < n; i++) {
+            for(s = i + 1; s < n; s++) {
+                if(arr[i] > arr[s]) {
+                    temp = arr[i];
+                    arr[i] = arr[s];
+                    arr[s] = temp;
+                }
+            }
+            System.out.println("phan tu " + i + ": " + arr[i]);
+        }
+     }
+}
