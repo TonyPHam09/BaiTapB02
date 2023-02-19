38bf9681275546225990df9f3633c8031d4d149b
TonyP <124651841+TonyPHam09@users.noreply.github.com>
2023-02-19 19:28:12 +0700

    Update cau4.java

--- cau4.java
+++ cau4.java
@@ -3,7 +3,8 @@ import java.lang.Math;
 
 public class cau4 {
      public static void main(String[] args) {
-        int n,i,s,temp,arr[];
+        int n,i,s,temp;
+        int[] arr;
         Scanner sc = new Scanner(System.in);
         System.out.printf("nhap mang: ");
         n = sc.nextInt();
